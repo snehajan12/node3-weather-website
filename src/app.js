0ac1ab50af43d71430146919398b1f5cabaa59c4
snehanp@qbytez.com <snehanp@qbytez.com>
2020-04-15 16:49:48 +0530

    Add about text

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 //DEFINE PATHS FOR EXPRESS CONFIG
 const publicDirectoryPath = path.join(__dirname, '../public')
